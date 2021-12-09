dff112d487a7ead730532ebe77fb754e6466cc7c
leonaysmael16 <ysmealleona@gmail.com>
2021-12-09 12:16:56 -0800

    added category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -38,15 +38,31 @@ router.post('/', (req, res) => {
   Category.create({
     category_name: req.body.category_name
   })
-  .then 
+  .then(categoryData => res.json(categoryData)) 
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(categoryData => res.json(categoryData))
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(categoryData => res.json(categoryData)) 
 });
 
 module.exports = router;
