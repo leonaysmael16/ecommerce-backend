c1effc66561b294c2440e8b536cd835b6ec9937f
leonaysmael16 <ysmealleona@gmail.com>
2021-12-09 11:54:42 -0800

    added columns

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,19 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "Tag",
+        key:'id'
+      }
+    }
   },
   {
     sequelize,
